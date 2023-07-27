d7974be333f014f4737bd175af9f61bba8dc2202
CarloCassini <c.cassini87@gmail.com>
2023-07-27 16:11:39 +0200

    add function random 1/5

--- js/function.js
+++ js/function.js
@@ -1,17 +1,6 @@
 console.log("function ok");
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++
-// esercizio 1
-let myButton1 = document.getElementById("esercizio-1");
-
-myButton1.addEventListener("click", function () {
-  console.log("esercizio 1");
-});
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++
-// esercizio 2
-let myButton2 = document.getElementById("esercizio-2");
-
-myButton2.addEventListener("click", function () {
-  console.log("esercizio 2");
-});
+function numeroRandom(min, max) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
 console.log("main ok");
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// esercizio 1
+let myButton1 = document.getElementById("esercizio-1");
+
+myButton1.addEventListener("click", function () {
+  console.log("esercizio 1");
+});
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// esercizio 2
+let myButton2 = document.getElementById("esercizio-2");
+
+myButton2.addEventListener("click", function () {
+  console.log("esercizio 2");
+
+  let numeroRandomPc = 0;
+  let numeroUtente;
+  let sceltaUtente;
+
+  let minimo = 1;
+  let massimo = 5;
+
+  numeroUtente = parseInt(prompt("scegli un numero da 1 a 5"));
+  if (isNaN(numeroUtente) || numeroUtente < 1 || numeroUtente > 5) {
+    alert("SCRIVI SOLO NUMERI DA 1 A 5");
+  } else {
+    // riduco sempre il testo al minuscolo
+    sceltaUtente = prompt("digita pari (p) o dispari (d)").toLowerCase();
+    if (sceltaUtente != "p" && sceltaUtente != "d") {
+      alert("la scelta pari e dispari accetta solo valori p e d");
+    } else {
+      numeroRandomPc = numeroRandom(minimo, massimo);
+
+      console.log("dopo " + numeroRandomPc);
+    }
+  }
+});
